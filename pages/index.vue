b98e1629ed02d9e623f3ddd45a684e2d974e142b
Ryan Steen <ryan@ryansteen.com>
2018-11-16 14:43:03 -0800

    Mobile adjustments

--- pages/index.vue
+++ pages/index.vue
@@ -17,7 +17,7 @@
       enter-active-class="animated zoomIn" 
       leave-active-class="animated zoomOut">
       <div v-if="show" class="about-content">
-        <b-card no-body class="text-center" style="max-height=80vh;">
+        <b-card no-body class="text-center">
         <About />
           This is some text within the default card block.
           <b-btn @click="show=!show, showButtons=!showButtons" class="btn-block">Back</b-btn>
@@ -143,7 +143,6 @@ export default {
   background-image: url("~assets/background.jpg");
   background-size: cover;
   border: solid rgb(56, 55, 55) 25px;
-  
 }
 .about-content {
   position: absolute;
@@ -154,14 +153,13 @@ export default {
   margin-top: -15%;
 }
 .center {
-    position: absolute;
     color: white;
-    width: 600px;
+    /* width: 600px;
     height: 100px;
     top: 50%;
     left: 50%;
     margin-top: -50px;
-    margin-left: -300px;
+    margin-left: -300px; */
 }
 
 .green-tree {
@@ -211,64 +209,78 @@ export default {
   background-color: yellow;
 }
 @media only screen and ( max-width: 500px ) {
-  .custom-button {
-  padding-top: 4px;
-  font-size: 1em;
-  text-align: center;
-  color: rgb(255, 251, 251);
-  background: rgba(0, 0, 0, 0.1);
-  width: 75px;
-  height: 30px;
-  border: 1px solid grey;
-  border-style: double;
-  transition: all .3s ease-in
-}
-.about-button {
-  margin-left: 5px;
-  position: absolute;
-  left: 0;
-  top: 1%;
-}
-.children-button {
-  margin-left: 5px;
-  position: absolute;
-  left: 0;
-  top: 94%;
-}
-.more-button {
-  margin-right: 5px;
-  position: absolute;
-  right: 0;
-  top: 1%;
-}
-.schedule-button {
-  margin-right: 5px;
-  position: absolute;
-  right: 0;
-  top: 94%;
-}
-.green-tree {
-  color: rgb(47, 170, 47);
-  padding-top: 20px;
-  font-size: 5em;
-}
-.center {
+  .home-page {
+    font-family: 'Oxygen', sans-serif;
+    overflow: hidden;
+    width: 100%;
     position: absolute;
-    color: white;
-    width: 400px;
-    height: 100px;
+    top: 0px;
+    bottom: 0px;
+    left: 0;
+    height: auto;
+    background-image: url("~assets/background.jpg");
+    background-size: cover;
+    border: solid rgb(56, 55, 55) 10px;
+  }
+  .custom-button {
+    padding-top: 4px;
+    font-size: 1em;
+    text-align: center;
+    color: rgb(255, 251, 251);
+    background: rgba(0, 0, 0, 0.1);
+    width: 75px;
+    height: 30px;
+    border: 1px solid grey;
+    border-style: double;
+    transition: all .3s ease-in
+  }
+  .about-button {
+    margin-left: 5px;
+    position: absolute;
+    left: 0;
+    top: 1%;
+  }
+  .children-button {
+    margin-left: 5px;
+    position: absolute;
+    left: 0;
+    top: 94%;
+  }
+  .more-button {
+    margin-right: 5px;
+    position: absolute;
+    right: 0;
+    top: 1%;
+  }
+  .schedule-button {
+    margin-right: 5px;
+    position: absolute;
+    right: 0;
+    top: 94%;
+  }
+  .green-tree {
+    color: rgb(47, 170, 47);
+    padding-top: 20px;
+    font-size: 4em;
+  }
+  .center {
+      /* position: absolute; */
+      margin-top:25vh;
+      color: white;
+      /* width: 400px;
+      height: 100px;
+      top: 50%;
+      left: 50%;
+      margin-top: -50px;
+      margin-left: -200px; */
+  }
+  .about-content {
+    position: absolute;
+    width: 90%;
     top: 50%;
     left: 50%;
-    margin-top: -50px;
-    margin-left: -200px;
-}
-.about-content {
-  position: absolute;
-  width: 90%;
-  top: 50%;
-  left: 50%;
-  margin-left: -45%; /* margin is -0.5 * dimension */
-  margin-top: -35%;
-}
+    margin-left: -45%; /* margin is -0.5 * dimension */
+    margin-top: -50%;
+  }
 }
 </style>
